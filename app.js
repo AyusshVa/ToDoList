434b5380e078acadeab1811ff410647f149bd6a8
AyusshVa <ayusshvashishth@gmail.com>
2022-03-07 14:31:31 +0530

    added a different work list about section and  header/footer layouts

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 app.set("view engine", "ejs");
 let items = [];
+let workItems = [];
 
 
 app.get("/", function(req, res){
@@ -17,15 +18,29 @@ app.get("/", function(req, res){
   
   };
   const info = d.toLocaleString("en-IN", options);
-  res.render("list", {day:info, temp:items})
+  res.render("list", {heading:info, newListItems:items})
 
 
 })
 
+app.get("/work", (req, res)=> {
+  let heading = "Work";
+  res.render("list", {heading: heading, newListItems: workItems})
+})
+
 app.post("/", (req, res)=>{
-const item = req.body.item;
-items.push(item);
-res.redirect("/");
+let item = req.body.item;
+if(req.body.list === "Work"){
+  workItems.push(item);
+  res.redirect("/work");
+}else{
+  items.push(item);
+  res.redirect("/")
+}
+})
+
+app.get("/about", (req, res)=>{
+  res.render("about");
 })
 
 app.listen("3000", ()=> {console.log("Server is running and up")})
