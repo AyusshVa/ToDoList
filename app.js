50370667ff5ad570ff7bcfffc7f2b5c691f6816e
AyusshVa <ayusshvashishth@gmail.com>
2022-04-16 18:25:40 +0530

    final commit after removing some minor bugs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static("public"))
 app.set("view engine", "ejs");
 
 // starting a mongoose connection.
-mongoose.connect("mongodb://localhost:27017/toDoListDB");
+mongoose.connect("mongodb+srv://admin-ayussh:test123@cluster0.t3mdo.mongodb.net/toDoListDB");
 
 // creating an items schema.
 
@@ -77,61 +77,64 @@ app.get("/", function (req, res) {
 
 // ***************************dynamic routes********************
 
-app.get('/:customListName', (req,res)=>{
+app.get('/:customListName',  (req,res)=>{
 const customListName =  _.capitalize(req.params.customListName);
 if(customListName === "About"){
   res.render("about");
 }
-else{
 
-  listModel.findOne({name: customListName}, (err,data)=>{
-    if(err) console.log("there is an error which is" + err);
-    else{
-      if(!data){
-        // make the list doc
-        var list1 = new listModel({
-          name: customListName,
-          items: [item1,item2,item3]
-        })
-        list1.save();
-        res.redirect("/" + customListName)
-      }
-  
+else{
+  if(customListName !== 'Favicon.ico'){
+    listModel.findOne({name: customListName}, (err,data)=>{
+      if(err) console.log("there is an error which is" + err);
       else{
-  
-        res.render("list", {
-          heading: customListName,
-          newListItems: data.items
-  
-          // **Note: after rendering any route, I don't know why its console.loging the favicon.ico in the terminal.
-        })
-       
+        if(!data){
+          // make the list doc
+          var list1 = new listModel({
+            name: customListName,
+            items: [item1,item3]
+          })
+          list1.save();
+          res.redirect("/" + customListName)
+        }
+    
+        else{
+    
+          res.render("list", {
+            heading: customListName,
+            newListItems: data.items
+    
+            // **Note: after rendering any route, I don't know why its console.loging the favicon.ico in the terminal.
+          })
+         
+        }
       }
-    }
-  });
+    });
+  }
+  
 }
 
 })
 
 // posting through home route. -> for addition to the list by adding to the database.
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   // store the item to store in var
   let item_var = req.body.item;
   let customListName = req.body.list;
  
-  var newItem = new itemModel({
+  var   newItem =   new itemModel({
     name: item_var
   });
 
   if(customListName === 'Today'){
-    newItem.save();
+    await newItem.save();
     res.redirect("/")
   }
 else{
-  listModel.findOne({name: customListName},  (err,data)=>{
+  listModel.findOne({name: customListName}, async (err,data)=>{
     data.items.push(newItem);
-    data.save();
+    await data.save();
     res.redirect("/" + customListName);
   });
 
@@ -142,7 +145,7 @@ else{
 
 
 // post for deletion 
-app.post("/delete", (req, res) => {
+app.post("/delete", async (req, res) => {
 
   let DeleItemId = req.body.checkbox;
   let listName = req.body.listName;
@@ -153,8 +156,9 @@ app.post("/delete", (req, res) => {
     }, (err) => {
       if (err) console.log(err);
       else console.log("Successfully deleted")
+      // ****************big bug resolved, the res.redirect was outside the deleteOne function therefore it was causing the issue of async.
+      res.redirect("/")
     });
-    res.redirect("/")
   }
 // deletion of items other than the today home list.
   else{
